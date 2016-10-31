8f271512ecabd418da5e4fa4dab8e83070029a88
nnthao <369874125>
2016-10-31 16:32:17 +0700

    2
    2

---
+++ SumTwoVariable/GanData/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GanData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = 1;
+            var a2 = 2;
+            var b3 = 3;
+            List<int> listInt = new List<int>{4,5,6};
+            for (int i = 0; i < listInt.Count; i++)
+            {
+              
+            }
+        }
+    }
+}
