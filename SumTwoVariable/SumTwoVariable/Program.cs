52dbd8b0d871fd85893bc9ef0495f4ac943f6c71
nnthao <369874125>
2016-10-31 11:40:24 +0700

    1
    1

--- SumTwoVariable/SumTwoVariable/Program.cs
+++ SumTwoVariable/SumTwoVariable/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DotLiquid;
 
 namespace SumTwoVariable
 {
@@ -10,8 +11,34 @@ namespace SumTwoVariable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!!");
+            PrameterData value = new PrameterData
+            {
+                name = "car",
+                my_name = "Thao"
+            };
+
+            var text = "This is my {{name}}\nMy name is {{my_name}}";
+            var data = (object) value;
+
+            var content = FormatTemplateWithContentTemplate(System.Net.WebUtility.HtmlDecode(text), data);           
+            Console.WriteLine(content);
             Console.ReadLine();
         }
+
+        public class PrameterData
+        {
+            public string name { get; set; }
+            public string my_name { get; set; }
+        }
+
+        public static string FormatTemplateWithContentTemplate(string contentTempalte, object data)
+        {
+            var template = Template.Parse(contentTempalte);
+            return template.Render(Hash.FromAnonymousObject(data));
+
+        }
+
     }
+
+
 }
