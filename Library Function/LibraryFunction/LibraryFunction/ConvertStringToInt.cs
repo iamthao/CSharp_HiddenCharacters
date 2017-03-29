93596dbced904ad6e2577cc999d37726d87e136c
nnthao <369874125>
2017-03-29 23:30:11 +0700

    1
    1

---
+++ Library Function/LibraryFunction/LibraryFunction/ConvertStringToInt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryFunction
+{
+    public class ConvertStringToInt
+    {
+        public static int TryParseIntFromStr(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return 0;
+
+            int valInt;
+            if (int.TryParse(input, out valInt))
+            {
+                return valInt;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Library Function/LibraryFunction/LibraryFunction/GetNumberFromString.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LibraryFunction
+{
+   public class GetNumberFromString
+    {
+       public static int GetNumberFromStr(string input)
+       {
+           return Convert.ToInt32(Regex.Replace(input, "[^0-9]+", string.Empty));
+       }
+    }
+}
