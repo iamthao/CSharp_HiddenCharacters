18fbc03637608621260c48f72dfbe11c19f199ea
nnthao <nnt.ntc@hotmail.com.vn>
2017-08-11 16:01:13 +0700

    ItextSharp Merger pdf
    ItextSharp Merger pdf

---
+++ CallPrinter/Test/MergePdf.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace Test
+{
+    class MergePdf
+    {
+        public static byte[] CombineMultiplePdfsByByte( List<byte[]> byteFiles)
+        {
+            var document = new Document(PageSize.A4, 0, 0, 0, 0);
+            var readerList = new List<PdfReader>();
+            var pathOutFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".pdf");
+            byte[] result = null;
+            try
+            {
+                foreach (var byteFile in byteFiles)
+                {
+                    var pdfReader = new PdfReader(byteFile);
+                    readerList.Add(pdfReader);
+                }
+
+                //Define a new output document and its size, type
+
+                //Create blank output pdf file and get the stream to write on it.
+
+                PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(pathOutFile, FileMode.Create));
+                document.Open();
+
+                foreach (PdfReader reader in readerList)
+                {
+                    for (int i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        PdfImportedPage page = writer.GetImportedPage(reader, i);
+                        document.Add(Image.GetInstance(page));
+                    }
+                }
+                document.Close();
+                foreach (PdfReader reader in readerList)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                }
+                result = File.ReadAllBytes(pathOutFile);
+            }
+            catch (Exception ex)
+            {
+                //PathTempAppHelper.WriteLog("CombineMultiplePdfs: " + ex.Message);
+                return null;
+            }
+            finally
+            {
+                File.Delete(pathOutFile);
+            }
+            return result;
+        }
+
+        public static void CombineMultiplePdfsByByteAndExport( List<byte[]> byteFiles, string desPath)
+        {
+            var document = new Document(PageSize.A4, 0, 0, 0, 0);
+            var readerList = new List<PdfReader>();
+            var pathOutFile = desPath;
+            try
+            {
+                readerList.AddRange(byteFiles.Select(byteFile => new PdfReader(byteFile)));
+
+                //Define a new output document and its size, type
+
+                //Create blank output pdf file and get the stream to write on it.
+
+                var writer = PdfWriter.GetInstance(document, new FileStream(pathOutFile, FileMode.Create));
+                document.Open();
+
+                foreach (var reader in readerList)
+                {
+                    for (var i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        var page = writer.GetImportedPage(reader, i);
+                        document.Add(Image.GetInstance(page));
+                    }
+                }
+                document.Close();
+                foreach (var reader in readerList)
+                {
+                    reader.Close();
+                    reader.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                //PathTempAppHelper.WriteLog("CombineMultiplePdfs: " + ex.Message);
+                //return null;
+            }
+
+        }
+    }
+}
---
+++ CallPrinter/Test/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listFileByte = new List<byte[]>();
+            var listFile = new List<string> { @"D:\TestCallPdf\1.pdf", @"D:\TestCallPdf\2.pdf" };
+
+            string fileName = Guid.NewGuid().ToString().Replace("-", "") + ".pdf";
+            var pathResult = Path.Combine(@"D:\TestCallPdf\", fileName);
+            Merge(listFile, pathResult);
+
+            //foreach (var item in listFile)
+            //{
+            //    listFileByte.Add(GetByteFromFile(item));
+            //}
+
+            //var bytesResult = MergePdf.CombineMultiplePdfsByByte(listFileByte);
+            
+            
+            //System.IO.File.WriteAllBytes(pathResult, bytesResult);
+
+            Console.WriteLine("Success!!!");
+            Console.ReadKey();
+        }
+
+        private static void SendToPrinter()
+        {
+            ProcessStartInfo info = new ProcessStartInfo(@"D:\pdf-test.pdf");
+            info.Verb = "PrintTo";
+            info.Arguments = "\"Foxit PhantomPDF Printer\"";
+            info.CreateNoWindow = true;
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            Process.Start(info);
+        }
+
+        public static byte[] GetByteFromFile(string path)
+        {
+            return System.IO.File.ReadAllBytes(path);
+        }
+
+        public static void Merge(List<String> InFiles, String OutFile)
+        {
+            using (FileStream stream = new FileStream(OutFile, FileMode.Create))
+            using (Document doc = new Document())
+            using (PdfCopy pdf = new PdfCopy(doc, stream))
+            {
+                doc.Open();
+
+                PdfReader reader = null;
+                PdfImportedPage page = null;
+
+                //fixed typo
+                InFiles.ForEach(file =>
+                {
+                    reader = new PdfReader(file);
+
+                    for (int i = 0; i < reader.NumberOfPages; i++)
+                    {
+                        page = pdf.GetImportedPage(reader, i + 1);
+                        pdf.AddPage(page);
+                    }
+
+                    pdf.FreeReader(reader);
+                    reader.Close();
+                });
+            }
+        }
+    }
+}
